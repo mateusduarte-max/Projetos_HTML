a39c796350dc29f650be92514e9923f923c3b4fd
Mateus Duarte <mateusduarte1984@gmail.com>
2021-05-05 14:32:43 -0300

    Curso - JavaScript

---
+++ Curso_JS/anotacoes.js
@@ -0,0 +1,5 @@
+// para converter para número utilizamos Number
+// para converter para String utilizamos String
+
+// ${variavel} template String, utilizamos para colocar exemplificar e informar a variavel a ser utilizada
+// Ex: window.alert(` A soma dos números ${n1} e ${n2} é igual a ${s}`) // utilizando template String
