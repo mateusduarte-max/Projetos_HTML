218a8ede57e1329670849f5f7ff54e77ba3da570
Mateus Duarte <mateusduarte1984@gmail.com>
2021-06-08 14:29:59 -0300

    Atualizar anotacoes.js

--- Curso_JS/anotacoes.js
+++ Curso_JS/anotacoes.js
@@ -3,3 +3,4 @@
 
 // ${variavel} template String, utilizamos para colocar exemplificar e informar a variavel a ser utilizada
 // Ex: window.alert(` A soma dos números ${n1} e ${n2} é igual a ${s}`) // utilizando template String
+
